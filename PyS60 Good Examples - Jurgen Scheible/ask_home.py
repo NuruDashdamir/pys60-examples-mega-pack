8dc129ee8e19071c584849372fa009c8f46379f5
NuruTasDemir <49725539+NuruTasDemir@users.noreply.github.com>
2022-01-04 21:15:04 +0400

    Update ask_home.py

--- PyS60 Good Examples - Jurgen Scheible/ask_home.py
+++ PyS60 Good Examples - Jurgen Scheible/ask_home.py
@@ -2,4 +2,4 @@ import appuifw
 planets = [ u'Mars', u'Earth', u'Venus' ]
 prompt = u'Enter your home planet'
 index = appuifw.popup_menu(planets, prompt)
-appuifw.note(u'Hello '+planets[index] , u'info')
+appuifw.note(u'Hello ' + planets[index], u'info')
